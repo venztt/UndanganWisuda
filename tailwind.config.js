68cfdaf201dbe68768fc86e6f526962a574383de
venztt <anthraxirfanprogamer@gmail.com>
2023-11-21 14:40:23 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        div_background: "url('../images/div_background.png')",
+      },
+    },
+  },
+  plugins: [],
+};
